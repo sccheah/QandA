589ed9533a8fc05cb32678444230c432c1b7d796
Stanley Cheah <sccheah@ucdavis.edu>
2019-01-10 11:35:20 -0800

    implement change password when logged in

--- QandA/settings.py
+++ QandA/settings.py
@@ -127,4 +127,3 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-# do password reset for login and password change when logged in

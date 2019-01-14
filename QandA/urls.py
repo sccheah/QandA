2e6af9a47eddb519983963514025b22e15c92983
Stanley Cheah <sccheah@ucdavis.edu>
2019-01-13 18:38:12 -0800

    include a post index page link in the base template

--- QandA/urls.py
+++ QandA/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
 	path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('posts/', include('posts.urls')),
+    path('posts/', include('posts.urls'), name='posts'),
     path('users/', include('users.urls')),
 	path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
